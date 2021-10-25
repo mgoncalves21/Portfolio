47eed02d41fcfd16d7047a2caa9890c6d59ce4f6
Morgan GONÇALVES <morgan.goncalves@diiage.org>
2021-10-25 13:27:10 +0200

    MyPortfolio 1.0

---
+++ myPortfolio/indexFR/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function (){
+    $(window).scroll(function (){
+        if (this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if (this.scrollY>500){
+            $('.scroll-up-btn').addClass("show");
+        }else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    //Slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+
+    //typing animation
+    var typed = new Typed(".typing",{
+        strings: ["FULLSTACK DEVELOPER", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
+    });
+    var typed = new Typed(".typing2",{
+        strings: ["IT development"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
+    });
+    var typed = new Typed(".typing3",{
+        strings: ["GitHub !"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function (){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
